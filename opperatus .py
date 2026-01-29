e90912a92c8cd58140e2074136a693445319329b
chandra-2309 <cchandra@student.gitam.edu>
2026-01-29 15:10:49 +0530

    Create opperatus .py

---
+++ opperatus .py
@@ -0,0 +1,36 @@
+a = 10
+b = 3
+
+addition = a + b
+print(f"Addition: {a} + {b} = {addition}")
+
+subtraction = a - b
+print(f"Subtraction: {a} - {b} = {subtraction}")
+
+# Multiplication
+multiplication = a * b
+print(f"Multiplication: {a} * {b} = {multiplication}")
+
+# Division
+division = a / b
+print(f"Division: {a} / {b} = {division}")
+
+# Floor Division
+floor_division = a // b
+print(f"Floor Division: {a} // {b} = {floor_division}")
+
+# Modulus
+modulus = a % b
+print(f"Modulus: {a} % {b} = {modulus}")
+
+# Exponentiation
+exponentiation = a ** b
+print(f"Exponentiation: {a} ** {b} = {exponentiation}")
+output :
+Addition: 10 + 3 = 13
+Subtraction: 10 - 3 = 7
+Multiplication: 10 * 3 = 30
+Division: 10 / 3 = 3.3333333333333335
+Floor Division: 10 // 3 = 3
+Modulus: 10 % 3 = 1
+Exponentiation: 10 ** 3 = 1000
