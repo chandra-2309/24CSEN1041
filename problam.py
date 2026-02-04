3dc12e350b08729a11eb21c7406a273ca8d96687
chandra-2309 <cchandra@student.gitam.edu>
2026-02-04 11:23:20 +0530

    Create problam.py

---
+++ problam.py
@@ -0,0 +1,20 @@
+# String slicing
+text = "problam"
+print("text =", text)
+
+print("text[0:5]   =", text[0:5])     # Prog
+print("text[4:]    =", text[4:])      # gramming
+print("text[:3]    =", text[:3])      # Progr
+print("text[-2:]   =", text[-2:])     # ming
+print("text[::4]   =", text[::4])     # Pormig
+print("text[::-2]  =", text[::-2])    # reverse
+print()
+output:
+text = problam
+text[0:5]   = probl
+text[4:]    = lam
+text[:3]    = pro
+text[-2:]   = am
+text[::4]   = pl
+text[::-2]  = mlop
+
