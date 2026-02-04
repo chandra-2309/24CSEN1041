45b482d3353fdc2e530b87e797fd207cccea150c
chandra-2309 <cchandra@student.gitam.edu>
2026-02-04 10:46:17 +0530

    Create string dhanush.py

---
+++ string dhanush.py
@@ -0,0 +1,20 @@
+s1 = "dhanush"
+s2 = 'python'
+s3 = """Welcome to string demo"""
+
+print("s1 =", s1)
+print("s2 =", s2)
+print("s3 =", s3)
+print()
+
+# Accessing strings (indexing)
+print("First character of s1:", s1[0])        # H
+print("Last character of s1:", s1[5])        # o
+print()
+output :
+s1 = dhanush
+s2 = python
+s3 = Welcome to string demo
+
+First character of s1: d
+Last character of s1: s
