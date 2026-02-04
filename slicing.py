fd9d037c299d5f4765693d0ac7ceea7777c7b034
chandra-2309 <cchandra@student.gitam.edu>
2026-02-04 11:18:39 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,20 @@
+# String slicing
+text = "Programming"
+print("text =", text)
+
+print("text[0:4]   =", text[0:4])     # Prog
+print("text[3:]    =", text[3:])      # gramming
+print("text[:5]    =", text[:5])      # Progr
+print("text[-4:]   =", text[-4:])     # ming
+print("text[::2]   =", text[::2])     # Pormig
+print("text[::-1]  =", text[::-1])    # reverse
+print()
+output:
+text = Programming
+text[0:4]   = Prog
+text[3:]    = gramming
+text[:5]    = Progr
+text[-4:]   = ming
+text[::2]   = Pormig
+text[::-1]  = gnimmargorP
+
